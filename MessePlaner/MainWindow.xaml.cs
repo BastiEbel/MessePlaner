711bdd6ba5d651f577308b8f44eceaa401bebc59
Sebastian Ebel <sebastianebel@hotmail.de>
2021-11-30 18:39:09 +0100

    Projektdateien hinzufügen.

---
+++ MessePlaner/MainWindow.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MessePlaner
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (TestdbEntities contex = new TestdbEntities())
+            {
+                var query = from exponat in contex.Exponat
+                            select new { exponat.Exponat_ID, exponat.Exponatname, exponat.Exponatnummer, exponat.Anzahl, exponat.Exponatversion, exponat.Exponatzubehör };
+                dgTest.ItemsSource = query.ToList();
+            }
+        }
+
+        private void btnBeenden_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Möchten sie Programm wirklich schließen?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    Application.Current.Shutdown();
+                }
+                else
+                {
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnBearbeiten_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnNeu_Click(object sender, RoutedEventArgs e)
+        {
+            Neu fensterneu = new Neu();
+            try
+            {
+                fensterneu.ShowDialog();
+                Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // über Combobox Datagrid Ansicht wechseln
+            using (TestdbEntities contex = new TestdbEntities())
+            {
+                contex.Database.Connection.Open();
+                try
+                {
+                    if (cbExponat.IsSelected)
+                    {
+                        var query = from exponat in contex.Exponat
+                                    select new { exponat.Exponat_ID, exponat.Exponatname, exponat.Exponatnummer, exponat.Exponatversion, exponat.Exponatzubehör };
+                        dgTest.ItemsSource = query.ToList();
+                        dgTest.Items.Refresh();
+                    }
+                    else if (cbMaße.IsSelected)
+                    {
+                        var query = from maße in contex.Maße
+                                    select new { maße.Maße_ID, maße.Länge_cm, maße.Breite_cm, maße.Höhe_cm, maße.Gewicht_kg };
+                        dgTest.ItemsSource = query.ToList();
+                        dgTest.Items.Refresh();
+                    }
+                    else if (cbZubehör.IsSelected)
+                    {
+                        var query = from zubehör in contex.Zubehör
+                                    select new { zubehör.Zubehör_ID, zubehör.Zubehörname, zubehör.Exponat_name, zubehör.Länge_cm, zubehör.Breite_cm, zubehör.Höhe_cm, zubehör.Gewicht_kg };
+                        dgTest.ItemsSource = query.ToList();
+                        dgTest.Items.Refresh();
+                    }
+                    else if (cbRetoure.IsSelected)
+                    {
+                        var query = from retoure in contex.Retoure
+                                    select new { retoure.Messe_ID, retoure.Messe, retoure.Land, retoure.Ort, retoure.Straße, retoure.Hausnummer, retoure.PLZ, retoure.Datum };
+                        dgTest.ItemsSource = query.ToList();
+                        dgTest.Items.Refresh();
+                    }
+                    else if (cbVersand.IsSelected)
+                    {
+                        var query = from versand in contex.Versand
+                                    select new { versand.Messe_ID, versand.Messe, versand.Land, versand.Ort, versand.Straße, versand.Hausnummer, versand.PLZ, versand.Datum };
+                        dgTest.ItemsSource = query.ToList();
+                        dgTest.Items.Refresh();
+                    }
+                    else if (cbAlle.IsSelected)
+                    {
+                        var query = from exponat in contex.Exponat
+                                    join maße in contex.Maße
+                                    on exponat.Exponat_ID equals maße.Exponat_ID
+                                    select new { exponat.Exponatname, exponat.Exponatnummer, exponat.Anzahl, exponat.Exponatzubehör, 
+                                                 maße.Länge_cm, maße.Breite_cm, maße.Höhe_cm, maße.Gewicht_kg };
+                        dgTest.ItemsSource = query.ToList();
+                        dgTest.Items.Refresh();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                contex.Database.Connection.Close();
+            }
+        }
+
+        private void btnSpeichern_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(btnNeu.IsEnabled == false)
+                {
+                    btnNeu.IsEnabled = true;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void tbSuche_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                //Suchabfrage über Textbox
+                using (TestdbEntities contex = new TestdbEntities())
+                {
+                    if (cbExponat.IsSelected == true)
+                    {
+                        var query = from exponat in contex.Exponat
+                                    select new { exponat.Exponat_ID, exponat.Exponatname, exponat.Exponatnummer, exponat.Anzahl, exponat.Exponatversion, exponat.Exponatzubehör };
+                       
+                        query = query.Where(exponat => exponat.Exponatname.Contains(tbSuche.Text));
+                        dgTest.ItemsSource = query.ToList();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ MessePlaner/Neu.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+
+namespace MessePlaner
+{
+    /// <summary>
+    /// Interaktionslogik für Neu.xaml
+    /// </summary>
+    public partial class Neu : Window
+    { 
+        public Neu()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbExponat.Focus();
+        }
+
+        private void btnSpeichern_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnSpeichern.IsFocused == true)
+            {
+                if (MessageBox.Show("Möchten sie die Eingabe speichern?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    using (TestdbEntities contex = new TestdbEntities())
+                    {
+                        try
+                        {
+                            var neuesExponat = new Exponat()
+                            {
+                                Exponatname = tbExponat.Text,
+                                Exponatnummer = Convert.ToInt32(tbNummer.Text),
+                                Exponatzubehör = tbZubehör.Text,
+                                Anzahl = Convert.ToInt32(tbAnzahl.Text),
+                                Exponatversion = Convert.ToDecimal(tbVersion.Text)
+                            };
+                            contex.Exponat.Add(neuesExponat);
+
+                            var neueMaße = new Maße()
+                            {
+                                Länge_cm = Convert.ToDecimal(tbLänge.Text),
+                                Breite_cm = Convert.ToDecimal(tbBreite.Text),
+                                Höhe_cm = Convert.ToDecimal(tbHöhe.Text),
+                                Gewicht_kg = Convert.ToDecimal(tbGewicht.Text)
+                            };
+                            contex.Maße.Add(neueMaße);
+                            
+                            var neuZubehör = new Zubehör()
+                            {
+                                Zubehörname = tbZubehörname.Text,
+                                Exponat_name = tbExponat.Text,
+                                Länge_cm = Convert.ToDecimal(tbZLänge.Text),
+                                Breite_cm = Convert.ToDecimal(tbZBreite.Text),
+                                Höhe_cm = Convert.ToDecimal(tbZHöhe.Text),
+                                Gewicht_kg = Convert.ToDecimal(tbZGewicht.Text),
+                                Anzahl = Convert.ToInt32(tbZAnzahl.Text)
+                            };
+                            contex.Zubehör.Add(neuZubehör);
+
+                            var neuVersand = new Versand()
+                            {
+                                Messe = tbMesse.Text,
+                                Land = tbLand.Text,
+                                Ort = tbOrt.Text,
+                                Straße = tbStrasse.Text,
+                                PLZ = tbPlz.Text,
+                                Hausnummer = tbHausnummer.Text,
+                                Datum = dpDatum.SelectedDate
+                            };
+                            contex.Versand.Add(neuVersand);
+
+                            var neuRetoure = new Retoure()
+                            {
+                                Messe = tbRMesse.Text,
+                                Land = tbRLand.Text,
+                                Ort = tbROrt.Text,
+                                Straße = tbRStrasse.Text,
+                                PLZ = tbRPlz.Text,
+                                Hausnummer = tbRHausnummer.Text,
+                                Datum = dpRDatum.SelectedDate
+                            };
+                            contex.Retoure.Add(neuRetoure);
+
+                            contex.SaveChanges();
+
+                            var textboxlist = new TextBox[] { tbAnzahl, tbBreite, tbExponat, tbExponatname, tbGewicht, tbHöhe, tbLänge, tbNummer, tbVersion, 
+                                                              tbZAnzahl, tbZBreite, tbZGewicht, tbZHöhe, tbZLänge, tbZubehör, tbMesse, tbLand, tbOrt, tbStrasse, tbPlz, tbHausnummer,
+                                                              tbRMesse, tbRLand, tbROrt, tbRStrasse, tbRPlz, tbRHausnummer };
+
+                            foreach (var tc in textboxlist)
+                            {
+                                tc.Clear();
+                            }
+                            return;
+                        }
+                        catch (Exception ex)
+                        {
+                            
+                            MessageBox.Show(ex.Message);
+                            
+                        }
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        private void btnSchliessen_Click(object sender, RoutedEventArgs e)
+        {
+            var textboxlist = new TextBox[] { tbExponat, tbNummer, tbAnzahl, tbVersion, tbLänge, tbBreite, tbHöhe, tbGewicht, tbExponatname, tbZubehörname, tbZAnzahl, tbZBreite, tbZGewicht, tbZHöhe, tbZLänge, tbZubehör };
+
+            foreach (var tb in textboxlist)
+            {
+                if (tb.Text != string.Empty)
+                {
+                    if (MessageBox.Show("Wenn sie das Fenster schließen, werden die eingegebenen Daten gelöscht", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                    {
+                        Close();
+                        break;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+            Close();
+        }
+
+        private void tbExponat_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tbExponatname.Text = tbExponat.Text;
+            tbZubehörname.Text = tbZubehör.Text;
+            tbRMesse.Text = tbMesse.Text;
+            tbRLand.Text = tbLand.Text;
+            tbROrt.Text = tbOrt.Text;
+            tbRStrasse.Text = tbStrasse.Text;
+            tbRPlz.Text = tbPlz.Text;
+            tbRHausnummer.Text = tbHausnummer.Text;
+
+            var textboxlist = new TextBox[] { tbAnzahl, tbBreite, tbExponat, tbExponatname, tbGewicht, tbHöhe, tbLänge, tbNummer, tbVersion,
+                                              tbZAnzahl, tbZBreite, tbZGewicht, tbZHöhe, tbZLänge, tbZubehör, tbMesse, tbLand, tbOrt, tbStrasse, tbPlz, tbHausnummer,
+                                              tbRMesse, tbRLand, tbROrt, tbRStrasse, tbRPlz, tbRHausnummer };
+
+            foreach (var tc in textboxlist)
+            {
+                if(String.IsNullOrEmpty(tc.Text))
+                {
+                    tc.Background = new SolidColorBrush(Color.FromArgb(23, 255, 0, 0));
+                    btnSpeichern.IsEnabled = false;
+                }
+                else
+                {
+                    tc.Background = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+                    btnSpeichern.IsEnabled = true;
+                }
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            using (TestdbEntities contex = new TestdbEntities())
+            {
+                var query = from exponat in contex.Exponat
+                            select new { exponat.Exponat_ID, exponat.Exponatname, exponat.Exponatnummer, exponat.Anzahl, exponat.Exponatversion, exponat.Exponatzubehör };
+                main.dgTest.ItemsSource = query.ToList();
+                main.dgTest.Items.Refresh();
+                main.Show();
+            }
+        }
+    }
+}
